774519187e20eb3fde178270df63d0100d4cd8d0
thongphungvan14 <84786251+thongphungvan14@users.noreply.github.com>
2021-11-13 11:02:12 +0700

    first

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -3,9 +3,10 @@ const { multipleMongooseToObject } = require('../../util/mongoose');
 class MeController {
     // [GET] /me/stored/courses
     storedCourses(req, res, next) {
-        Course.find({})
-            .then((courses) =>
+        Promise.all([Course.find({}), Course.countDocumentsDeleted()])
+            .then(([courses, deletedCount]) =>
                 res.render('me/stored-Courses', {
+                    deletedCount,
                     courses: multipleMongooseToObject(courses),
                 }),
             )
